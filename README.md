2dd03c2041ccb1857aeb836edcdf0ae7efe3ecd5
Ian Sawicki <iansawicki@isawicki-mbp.local>
2023-10-01 10:28:00 -0700

    more flow tweaks

--- README.md
+++ README.md
@@ -8,6 +8,12 @@ We strongly recommend you read our [docs](https://docs.octoai.cloud/docs), which
 - [Python SDK Examples](https://docs.octoai.cloud/docs/use-the-python-client-for-templates)
 - [OctoAI Home](https://octoai.cloud/)
 
+
+### Document Overview
+- [Quick Start & Basic Usage](#quick-start--basic-usage)
+- [Explore the Image Generation SDXL API with Streamlit](#explore-the-image-generation-sdxl-api-with-streamlit)
+- [OctoAI Image Generation API Reference](#octoai-image-generation-api-reference)
+
 ## Quick Start & Basic Usage
 
 Install the project requirements and set your environment variables. Then, run the cURL command below to generate an image. (We will share your token separately.) 
@@ -115,7 +121,7 @@ else:
 
 ```
 
-## Advance OctoAI Usage, Customization, and SDK
+## Explore the Image Generation SDXL API with Streamlit
 To make exploring the OctoAI service easier, launch the Streamlit app included in this reposotiry. 
 
 ```bash
@@ -124,30 +130,128 @@ streamlit run app/1_OctoAI_Eval.py
 
 This will launch a Streamlit app that will allow you to explore all the functionality of the OctoAI service, including customization features, such as loras, negative prompts, checkpoints, and more.
 
-## OctoAI Image Generation API Specs
+## OctoAI Image Generation API Reference
+This document describes the arguments you can provide to the Image Generation API. It supports a rich set of configurations for generating images.
 
-We support the following dimensions for SDXL
+### `prompt`
+- **Type**: `string`
+- **Description**: Describes the image to generate.
+- **Constraints**:
+    - Maximum of 77 tokens.
+    - Supports prompt weighting. For example: `(A tall (beautiful:1.5) woman:1.0) (some other prompt with weight:0.8)`.
+    - Weight is the product of all brackets a token is in. The brackets, colons, and weights don't count towards the token count.
 
-Height | Width 
------------- | -------------
-1024 | 1024
-1152 | 896
-1152 | 896
-1152 | 896
-1152 | 896
-1152 | 896
+### `prompt_2`
+- **Type**: `string`
+- **Description**: 
+    - Default mode (when only `prompt` is set): Input of `prompt` is copied to `prompt_2`.
+    - Otherwise, `prompt` is used for "word salad" style control, and `prompt_2` is for more human-readable descriptions.
+- **Examples**:
+    - `prompt = "photorealistic, high definition, masterpiece, sharp lines"`
+    - `prompt_2 = "A portrait of a handsome cat wearing a little hat. The cat is in front of a colorful background."`
+
+### `negative_prompt` (Optional)
+- **Type**: `string`
+- **Description**: Steer away from certain themes.
+
+### `sampler` (Optional)
+- **Type**: `string`
+- **Description**: Specifies which scheduler to use for image generation.
+- **Default**: `DDIM`
+- **Available Values**:
+    - `PNDM`: `PNDMScheduler`
+    - `KLMS`: `LMSDiscreteScheduler`
+    - `DDIM`: `DDIMScheduler`
+    - `DDPM`: `DDPMScheduler`
+    - `K_EULER`: `EulerDiscreteScheduler`
+    - `K_EULER_ANCESTRAL`: `EulerAncestralDiscreteScheduler`
+    - `DPMSolverMultistep`: ["DPMSolverMultistepScheduler", {"use_karras_sigmas": False}]
+    - `K_DPMPP_2M`: `["DPMSolverMultistepScheduler", {"use_karras_sigmas": True}]
+    - `DPM++2MKarras`: `DPMSolverMultistepScheduler`
+    - `KLMS`: `LMSDiscreteScheduler`
+    - `DPMSingle`: `DPMSolverSinglestepScheduler`
+    - `HEUN`: `HeunDiscreteScheduler`
+    - `DPM_2`: `KDPM2DiscreteScheduler`
+    - `DPM2_ANCESTRAL`: `KDPM2AncestralDiscreteScheduler`
+    - `DPM++ SDE Karras`: [DPMSolverSDEScheduler, {"use_karras_sigmas": True}]
+
+### `height` (Optional)
+- **Type**: `int`
+- **Description**: Specifies the height of the output image.
+- **Default**: `1024`
+
+### `width` (Optional)
+- **Type**: `int`
+- **Description**: Specifies the width of the output image.
+- **Default**: `1024`
+
+### Supported Resolutions
+
+| Resolution | Aspect Ratio |
+|------------|--------------|
+| 1024x1024  | 1:1          |
+| 1152x896   | 4:3          |
+| 896x1152   | 3:4          |
+| 1216x832   | 4:3          |
+| 896x1152   | 3:4          |
+| 832x1216   | 3:4          |
+| 1344x768   | 16:9         |
+| 768x1344   | 9:16         |
+| 1536x640   | 12:5         |
+| 640x1536   | 5:12         |
 
 
-1024x1024
+### `cfg_scale` (Optional)
+- **Type**: `float`/`int`
+- **Description**: How strictly the process adheres to the prompt.
+- **Default**: `12`
 
-1152x896
-896x1152
+### `steps` (Optional)
+- **Type**: `int`
+- **Description**: Number of diffusion steps.
+- **Default**: `30`
 
-1216x832
-832x1216
+### `num_images`
+- **Type**: `int`
+- **Description**: Number of images to generate.
 
-1344x768
-768x1344
+### `seed` (Optional)
+- **Type**: `int`
+- **Description**: Fixes random noise for replicable results.
+- **Default**: Random
 
-1536x640
-640x1536# typeface-external
+### `style_preset` (Optional)
+- **Type**: `string`
+- **Description**: Guides the output image style.
+- **Available Values**: `3d-model`, `analog-film`, `anime`, ... (include all other styles here)
+
+### `use_refiner`
+- **Type**: `bool`
+- **Description**: Use the refiner or not.
+
+### `high_noise_frac` (Optional)
+- **Type**: `float`/`int`
+- **Description**: Determines noise amount using the base model vs. refiner.
+- **Default**: `0.8`
+
+### `model`
+- **Type**: `string`
+- **Description**: Checkpoints supported.
+- **Available Values**: `copax-timeless`, `crystal-clear`, ... (include all other models here)
+
+### `loras`
+- **Type**: `string`
+- **Description**: LoRAs supported in name-weight pairs.
+- **Available Values**: `add-detail`, `crayon-style`, ... (include all other LoRAs here)
+
+### `init_image` (Optional)
+- **Type**: `string`
+- **Description**: Use an image as a starting point. Encoded in base64.
+
+### `strength` (Optional)
+- **Type**: `float`/`int`
+- **Description**: Noise amount for img2img use cases.
+- **Default**: `0.8`
+
+## Notes
+- **Img2Img Cases**: `init_image` and `strength` are applicable only for Img2Img cases.
--- app/1_OctoAI_Eval.py
+++ app/1_OctoAI_Eval.py
@@ -5,11 +5,12 @@ from dotenv import load_dotenv
 import os
 from pathlib import Path
 from pydantic import BaseModel
-from typing import List
+from typing import List, Dict
 import json
 from hashlib import sha256
 import yaml
 from octoai.types import Image
+st.set_page_config(layout="wide")
 
 BASE_PATH = Path(__file__).parent
 ENV_PATH = BASE_PATH / "conf.yaml"
@@ -18,7 +19,7 @@ CONF = yaml.safe_load(ENV_PATH.open())
 IMAGES_PATH = BASE_PATH / "generated_images"
 
 # Create a directory to store the images
-IMAGES_PATH.mkdir(parents=True, exist_ok=False)
+IMAGES_PATH.mkdir(parents=True, exist_ok=True)
 
 # Load token from yaml file
 OCTOAI_TOKEN = CONF["token"]
@@ -27,13 +28,9 @@ client = Client(token=OCTOAI_TOKEN)
 endpoint = OCTOAI_ENDPOINT + "/predict"
 healthcheck = OCTOAI_ENDPOINT + "/healthcheck"
 
-# Pydantic schema for the request body
-class LorasConfig(BaseModel):
-    crayon_style: float
-    paint_splash: float
-    
 class Config(BaseModel):
     prompt: str
+    prompt_2: str
     negative_prompt: str
     sampler: str
     cfg_scale: float
@@ -46,24 +43,31 @@ class Config(BaseModel):
     strength: float
     use_refiner: bool
     model: str # coming soon
-    # loras: LorasConfig # coming soon
+    loras: Dict[str, float] # coming soon
     
 # Create an instance of the Config Pydantic model with default values
+
 config = Config(
     prompt="a cat in a fishbowl hyperrealistic painting",
+    prompt_2="a happy little cat in a fishbowl with happy little fish",
     negative_prompt="ugly, tiling, poorly drawn hands, poorly drawn feet, poorly drawn face, out of frame, extra limbs, disfigured, deformed, body out of frame, blurry, bad anatomy, blurred, watermark, grainy, signature, cut off, draft",
     sampler="DPM++2MKarras",
     cfg_scale=7.5,
     height=1024,
     width=1024,
-    seed=0,
+    seed=42,
     steps=20,
     num_images=1,
     high_noise_frac=0.92,
     strength=0.92,
     use_refiner=True,
-    model=""
+    model="default",
+    loras={
+        "crayon-style": 0.0,
+        "paint-splash": 0.0
+    }
 )
+
 # Generic helper funcs
 
 # Run inference with the OctoAI SDK
@@ -89,7 +93,7 @@ def eg_octo_inference(input_payload, endpoint=endpoint, healthcheck=healthcheck)
             
             # Generate the output file name with the numerical identifier
             output_file_name = str(IMAGES_PATH / f"{payload_id}_octo_{i}.png")
-            st.write("Saving image to", output_file_name)
+            #st.write("Saving image to", output_file_name)
             
             # Convert and save the base64 image with PIL
             Image.from_base64(image_data).to_file(output_file_name)
@@ -136,54 +140,37 @@ def sd_inputs():
     with col1:
 
         config.prompt = st.text_input("Prompt", config.prompt)
+        config.prompt_2 = st.text_input("Prompt 2", config.prompt_2)
         config.negative_prompt = st.text_input("Negative Prompt", config.negative_prompt)
-        config.sampler = st.selectbox("Sampler",["PNDM", "KLMS", "DDIM", "DDPM", "K_EULER", "K_EULER_ANCESTRAL", "DPMSolverMultistep", "DPM++2MKarras", "DPMSingle", "HEUN", "DPM_2", "DPM2_ANCESTRAL"])
-        config.cfg_scale = st.number_input("CFG Scale", config.cfg_scale,step=0.5)
+        config.sampler = st.selectbox("Sampler",["DPM++2MKarras", "KLMS", "DDIM", "DDPM", "K_EULER", "K_EULER_ANCESTRAL", "DPMSolverMultistep", "PNDM", "DPMSingle", "HEUN", "DPM_2", "DPM2_ANCESTRAL"])
+        config.cfg_scale = st.slider("CFG Scale",step=0.5,value=config.cfg_scale,max_value=30.0)
         config.height = st.number_input("Height", config.height)
         config.width = st.number_input("Width", config.width)
         
     with col2:
-        config.seed = st.number_input("Seed", config.seed)
-        config.steps = st.number_input("Steps", config.steps)
+        config.seed = st.number_input("Seed", value=config.seed)
+        config.steps = st.slider("Steps", value=config.steps,step=1,max_value=100)
         config.num_images = st.number_input("Number of Images", config.num_images)
-        config.high_noise_frac = st.number_input("High Noise Fraction", config.high_noise_frac)
+        config.high_noise_frac = st.slider("High Noise Fraction", value=config.high_noise_frac,step=0.1,max_value=1.0)
         config.strength = st.number_input("Strength", config.strength)
         config.use_refiner = st.checkbox("Use Refiner", config.use_refiner)
-        #config.model = st.text_input("Model", config.model)
-
+        config.model = st.selectbox("Custom Checkpoints", ["default","copax-timeless", "crystal-clear", "duchaiten-aiart", "realcartoon", "samaritan"])
 
 def sd_inputs_loras():
-    return
+    config.loras["crayon-style"] = st.slider("Crayon Style", config.loras["crayon-style"],step=0.1)
+    config.loras["paint-splash"] = st.slider("Paint Splash", config.loras["paint-splash"],step=0.1)
+
 
     #config.loras.crayon_style = st.number_input("Crayon Style", config.loras.crayon_style)
     #config.loras.paint_splash = st.number_input("Paint Splash", config.loras.paint_splash)
         
     # Display the updated config
-    
-      
-def config_payload():
-    # Assuming you already have the `config` Pydantic model instance
-    json_payload = config.dict()
 
-    # Convert the Python dictionary to a JSON string
-    json_payload_str = json.dumps(json_payload, indent=4)
-    return json_payload_str
-        
-    
+# Wrapper func to update data configuration as changes made
 
-
-# Image styling section
-def section_three(inference_button):
-    
-    
-    if inference_button:
-        updated_config_params = config.dict()
-        st.write(updated_config_params)
-        img_res = eg_octo_inference(config.dict())
-        # Display the image
-        st.image(img_res, width=400)
 
 def main():
+
     st.title('OctoAI Imagen & SDXL Evaluation | Typeface')
     # Global controls, config, and variables
     # Setup the sidebar
@@ -204,74 +191,99 @@ def main():
             yaml.dump(CONF, f)
     
     inference_button = st.sidebar.button("Run Inference")
-
-    # Layout
-
-    tab1,tab2 = st.tabs(["Text2Image","ControlNet - Coming Soon"])
-    with tab1:
-        # Display config params
-        sd_inputs()
-        
-        if inference_button:
-            updated_config_params = config.dict()
-            st.write(updated_config_params)
-            img_res = eg_octo_inference(config.dict())
-            # Display the image
-            st.image(img_res, width=400,use_column_width="auto")
-            # Show tab2 by default
     
+    # Layout
+    
+    col1, col2 = st.columns([0.4, 0.6])
+    # Container 1
+    with col2:
+        container1 = st.container()
+
+        tab1,tab2 = container1.tabs(["Text2Image","Lora Configs"])
+        with tab1:
+            # Display config params
+            sd_inputs()
+            
+        with tab2:
+            # Display config params
+            sd_inputs_loras()
+            
+    img_placeholder = container1.empty()
+        
+    if inference_button:
+
+        # Run inference
+        # Drop model key if equal 'default'
+        payload = config.dict()
+        if payload["model"] == "default":
+            del payload["model"]
+            del payload["loras"]
+        #st.code(payload)
+        img_res = eg_octo_inference(payload)
+        
+        # Update placeholder with image
+        img_placeholder.image(img_res, width=300)
+                
+    container1.write(config.dict())
+            
     # Sections
     # Section 1
-    st.header("Quickstart")
-    st.write(""" Let's run a few basic queries against the OctoML SDXL 
-    endpoint you were provided to make sure it's working correclty. 
-    I've provided some sample code with SDXL queries below. You can press:
-    'run code' to see the code go.""")
-    st.subheader("1. Endpoint Test")
-    st.write(""" 
-             To get started, ensure you've set your token and endpoint. You can paste your
-             token in the sidebar and press 'enter.'
-             
-             Copy and paste the curl command below into your terminal to confirm your OctoAI SDXL
-             endpoint is ready to accept resuts. 
-             """)
-    new_payload = json.dumps(config.dict(),indent=4)
+    with col1:
+        st.header("Quickstart")
+        st.write(""" To get started, please see the sample scenarios below. As you adjust your SDXL parameters, the request payload will automatically update.
+                 Press the _Run Inference_ button to generate an example images from.
+        """)
+        st.subheader("1. Endpoint Test")
+        st.write(""" 
+                To get started, ensure you've set your token and endpoint. You can paste your
+                token in the sidebar and press 'enter.'
+                
+                Copy and paste the curl command below into your terminal to confirm your OctoAI SDXL
+                endpoint is ready to accept resuts. 
+                """)
+        updated_config_params = json.dumps(config.dict(),indent=4)
 
-    st.code(f"""
-            curl -H 'Content-Type: application/json'\
-                 -H 'Authorization: Bearer {OCTOAI_TOKEN}'\
-                 -X POST 'https://image.octoai.run/predict'\
-                 -d '{new_payload}'
-                 """, language="bash")
-    
-    # Section 2
-    
-    st.subheader("2. The OctoML SDK")
-    st.markdown(""" Use the OctoML SDK run inferences against SDXL endpoint in a more
-             repeatable, programmable, and CI/CD friendly way.
-             If you haven't already, install the OctoML SDK with:""")
-    st.code("pip install octoai", language="bash")
-    # Run inference with the OctoAI SDK
-    new_payload = json.dumps(config.dict(),indent=4)
-    
-    # Section 3 - Try another one
-    st.subheader("3. A different result")
-    st.write(""" 
-             Update the parameters to change the image. Try something like: 'A cat in a fishbowl'
-                and see what happens. You can also change the sampler to 'DPM++2MKarras' to get a different results
-             """)
-    
-    # Section 4 - Multiple images
-    st.subheader("4. Multiple images")
-    st.write(""" 
-             You can also generate multiple images at once. Try changing the number of images to 3.
-             """)
-    
-    
-    with tab2:
-        sd_inputs()
+        st.code(f"""
+                curl -H 'Content-Type: application/json'\
+                    -H 'Authorization: Bearer {OCTOAI_TOKEN}'\
+                    -X POST 'https://image.octoai.run/predict'\
+                    -d '{updated_config_params}'
+                    """, language="bash")
+        
+        # Section 2 - Try another one
+        st.subheader("A custom result")
+        st.write(""" 
+                Update the parameters to change the image. 
+                * Try something like: ```'A cat in a fishbowl swimming with goldfish hyperrealism'```
+                * Change the sampler (scheduler) to `K_EULER`.
+                * Change the number of steps to `30`.
+                """)
+        
+        # Section 3 - Multiple images
+        st.subheader("Multiple images")
+        st.write(""" 
+                You can also generate multiple images at once. Try changing the number of images to `3`.
+                """)
+        
+        # Seection 4 - Custom checkpoints
+        st.subheader("Custom checkpoints")
+        st.write(""" 
+                You can generate images from custom checkpoints. Change the model setting to ```copax-timeless``` and click the _Run Inference_ button.
+                """)
+        
+        # Section 5 - Loras
+        st.subheader("Using LoRAs")
+        st.write(""" 
+               Apply a LoRA to guide the output image style from the `Lora Configs` tab. Try changing the ```crayon-style``` and ```paint-splash``` settings to `0.7` and `0.3` respectively.
+                """)
+        
+        # Section 5 - Full API reference
+        st.subheader("Full API reference")
+        st.write(""" 
+                Navigate to the full SDXL Image Gen API refernece from the sidebar.
+                """)
 
-        st.write("Coming soon")
+    
             
 if __name__ == "__main__":
     main()
